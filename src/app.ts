c4b38ea2bbdf73b41e93a3d26687564b2be23c59
ErianVT <erianvieira.tome@yahoo.com>
2025-10-16 06:57:24 -0300

    conectando api ao front-end e adicionando CORS

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@ import { env } from "node:process";
 import type { FastifyInstance } from "fastify";
 import Fastify from "fastify";
 import routes from "./routes";
+import cors from "@fastify/cors";
 
 const app: FastifyInstance = Fastify({
   logger: {
     level: env.NODE_ENV === "dev" ? "info" : "error",
   },
 });
+
+app.register(cors);
 
 app.register(routes, { prefix: "/api" });
 
